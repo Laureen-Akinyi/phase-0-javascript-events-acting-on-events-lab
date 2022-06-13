6067a3388f75bc97a23517295038c8526863b8d8
laureenakinyi <laureen.akinyi@student.moringaschool.com>
2022-06-13 23:14:37 +0300

    commit tree ac08321590aea40738b3c861ab53c0c486e6cfd6

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+// const dodger = document.getElementById("dodger");
+// dodger.style.backgroundColor = "#000000";
+// dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "100px";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+
+var dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left <  360) {
+      dodger.style.left = `${left + 1}px`;
+    }  
+}
+document.addEventListener("keydown", function (e){
+    if (e.key === "ArrowRight") {
+      moveDodgerLeft();
+    }
+    });
+
+    document.addEventListener("keydown", function (e){
+        if (e.key === "ArrowRight") {
+          moveDodgerRight();
+        }
+        })
+    
